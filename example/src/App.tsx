e3a878e56117b5cfc206a565426a990b153a1518
mohasinac <mohsin.ahamed@hotmail.com>
2021-09-23 21:56:54 +0530

    library created

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-doc-viewer'
+import 'react-doc-viewer/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
